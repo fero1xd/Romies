6f235ddac72a95194af0303caa9395c17599cb94
FeroniK <71776171+FeroniK@users.noreply.github.com>
2021-08-26 14:45:49 +0530

    Updated email.js

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = process.env.EMAIL_FROM || 'pranjaldummy@gmail.com';
+    this.from = process.env.EMAIL_FROM || 'YOUR_EMAIL';
   }
 
   newTransport() {
